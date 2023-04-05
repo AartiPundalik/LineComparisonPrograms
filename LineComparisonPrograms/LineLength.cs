ba661b1f04378df6ef97d8c33e792cbbc479d8f4
AartiPundalik <aartipundalik68@gmail.com>
2023-04-05 18:17:53 +0530

    [Aarti] Add : UC1 - Line Of Length

---
+++ LineComparisonPrograms/LineLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonPrograms
+{
+    public class LineLength
+    {
+        int lengthX, _lengthY, lenGthsX, lengthsY;
+        public LineLength(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine("Lenght of the line is : " + length);
+            return length;
+        }
+    }
+}
+
---
+++ LineComparisonPrograms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LineLength length = new LineLength(4, 8, 3, 5);
+            length.Calculate();
+            Console.ReadLine();
+        }
+    }
+}
