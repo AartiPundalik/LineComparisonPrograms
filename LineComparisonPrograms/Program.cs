9021c3e645b36c5385170911aedadeffeb48944c
AartiPundalik <aartipundalik68@gmail.com>
2023-04-05 18:32:45 +0530

    [Aarti] Add : UC3 - Comparing The Two Lines

--- LineComparisonPrograms/Program.cs
+++ LineComparisonPrograms/Program.cs
@@ -14,18 +14,35 @@ namespace LineComparisonPrograms
             //length.Calculate();
             //Console.ReadLine();
 
-            EqualityOfLines length_One = new EqualityOfLines(4, 2, 7, 3);
-            double Length = length_One.CalculateOne();
+            //EqualityOfLines length_One = new EqualityOfLines(4, 2, 7, 3);        \\UC2
+            //double Length = length_One.CalculateOne();
 
-            EqualityOfLines length_Two = new EqualityOfLines(5, 8, 4, 6);
-            double length_ = length_Two.CalculateTwo();
+            //EqualityOfLines length_Two = new EqualityOfLines(5, 8, 4, 6);
+            //double length_ = length_Two.CalculateTwo();
 
-            if (Length.Equals(length_))
-                Console.WriteLine("Both line are equal");
+            //if (Length.Equals(length_))
+            //    Console.WriteLine("Both line are equal");
+            //else
+            //    Console.WriteLine("Both lines are not equal");
+
+            //Console.ReadLine();
+
+            CompareTwoLines Length_One = new CompareTwoLines(4, 2, 7, 3);
+            double length = Length_One.CalculateOne();
+
+            CompareTwoLines Length_Two = new CompareTwoLines(5, 4, 5, 6);
+            double Length_ = Length_Two.CalculateTwo();
+
+            if (length.CompareTo(Length_) == 0)
+                Console.WriteLine("Both lines are equal ");
+            else if (length.CompareTo(Length_) > 0)
+                Console.WriteLine("Line one is greater");
             else
-                Console.WriteLine("Both lines are not equal");
-
+                Console.WriteLine("Line two is greater");
             Console.ReadLine();
         }
     }
 }
+        
+    
+
