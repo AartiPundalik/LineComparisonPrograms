29f810b364ab850e11962c1ba1fe520832bfebcb
AartiPundalik <aartipundalik68@gmail.com>
2023-04-05 18:25:28 +0530

    [Aarti] Add : UC2 - Checking Equality Of Lines

--- LineComparisonPrograms/Program.cs
+++ LineComparisonPrograms/Program.cs
@@ -10,8 +10,21 @@ namespace LineComparisonPrograms
     {
         static void Main(string[] args)
         {
-            LineLength length = new LineLength(4, 8, 3, 5);
-            length.Calculate();
+            //LineLength length = new LineLength(4, 8, 3, 5);       \\UC1
+            //length.Calculate();
+            //Console.ReadLine();
+
+            EqualityOfLines length_One = new EqualityOfLines(4, 2, 7, 3);
+            double Length = length_One.CalculateOne();
+
+            EqualityOfLines length_Two = new EqualityOfLines(5, 8, 4, 6);
+            double length_ = length_Two.CalculateTwo();
+
+            if (Length.Equals(length_))
+                Console.WriteLine("Both line are equal");
+            else
+                Console.WriteLine("Both lines are not equal");
+
             Console.ReadLine();
         }
     }
